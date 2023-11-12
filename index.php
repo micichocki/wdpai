aac03bcf5fd65709cf6cae5ec425cf5b71a8b4a0
Michal Cichocki <cichockimichal849@gmail.com>
2023-11-12 15:54:08 +0100

    Adds basic routing and basic css

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
 
-echo 'Hi there 👋';
+require 'Routing.php';
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url( $path, PHP_URL_PATH);
+
+Router::get('', 'DefaultController');
+Router::get('login', 'DefaultController');
+
+Router::run($path);
